3932c2f76a92f3808fea40254475a2b8054e5c33
grungies1138 <grungies1138@gmail.com>
2018-08-22 13:20:02 -0500

    Finishing SheetCommand

--- commands/chargen_commands.py
+++ commands/chargen_commands.py
@@ -153,16 +153,16 @@ def chargen_personal(caller):
 
     text += "\n|wHair:|n {}".format(HAIR.get(caller.db.hair))
 
-    # text += "\n|wHeight:|n {}".format(caller.db.height)
-    #
-    # text += "\n|wWeight:|n {}".format(caller.db.weight)
-    #
-    # text += "\n|wSkin:|n {}".format(SKIN[caller.db.skin])
-    #
-    # text += "\n|wPersonality:|n {}".format(", ".join(caller.db.personality))
-    #
-    # text+= "\n|wGender:|n {}".format(caller.db.gender)
-    #
+    text += "\n|wHeight:|n {}".format(caller.db.height)
+
+    text += "\n|wWeight:|n {}".format(caller.db.weight)
+
+    text += "\n|wSkin:|n {}".format(SKIN[caller.db.skin])
+
+    text += "\n|wPersonality:|n {}".format(", ".join(caller.db.personality))
+
+    text+= "\n|wGender:|n {}".format(caller.db.gender)
+
     # text += "\n|wHome Sector:|n {}".format(caller.db.sector)
     #
     # text += "\n\nPlease select an option to customize."
