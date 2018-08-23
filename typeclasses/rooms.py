070150a8f656d54bf18c15eab89a739e132e9215
grungies1138 <grungies1138@gmail.com>
2018-08-23 16:41:14 -0500

    Finishing SheetCommand

--- typeclasses/rooms.py
+++ typeclasses/rooms.py
@@ -25,9 +25,9 @@ class Room(DefaultRoom):
     def return_appearance(self, looker):
         message = []
         # message.append(header(self.key))
-        message.append("|w-|n" * 78)
+        message.append("|w_|n" * 78)
         message.append("|[002|w{}{}|n".format(self.key, " " * (78 - len(self.key))))
-        message.append("|w-|n" * 78)
+        # message.append("|w-|n" * 78)
         message.append(self.db.desc)
         message.append(header())
 
