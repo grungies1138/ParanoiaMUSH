3012e0558b69e7a7810ae59db075ee2b839ebac4
grungies1138 <grungies1138@gmail.com>
2018-08-27 08:52:21 -0500

    Finishing touches on chargen

--- typeclasses/rooms.py
+++ typeclasses/rooms.py
@@ -26,40 +26,39 @@ class Room(DefaultRoom):
     def at_object_creation(self):
         self.db.clearance = 1
 
-    #def return_appearance(self, looker):
-        # message = []
-        # message.append(header(self.key))
-        # message.append("|w_|n" * 78)
-        # message.append("|[002|w{}{}|n\n".format(self.key, " " * (78 - len(self.key))))
-        # message.append("|w-|n" * 78)
-        # message.append(self.db.desc)
-        # message.append("|{}_|n".format(clearance_color(CLEARANCE.get(self.db.clearance))) * 78)
-        #
-        # chars = self.list_characters()
-        # objects = self.list_non_characters()
-        # colored_objects = []
-        # for obj in objects:
-        #     colored_objects.append("|135%s|n" % obj.key)
-        #
-        # exits = []
-        # if self.exits:
-        #     for exit in self.exits:
-        #         if exit.access(looker, "view"):
-        #             exits.append("|w<|n|b%s|n|w>|n - %s" % (exit.key, exit.destination))
-        #
-        # table = evtable.EvTable("|wCharacters and Objects:|n", "|wExits:|n", table=[chars + colored_objects, exits],
-        #                         border=None)
-        # table.reformat_column(0, width=28, align="l")
-        #
-        # message.append(table)
-        # message.append("\n")
-        #
-        # message2 = []
-        # for line in message:
-        #     message2.append(unicode(line))
-        #
-        # return "\n".join(message2)
-        #return "{}\n{}\n".format(self.key, self.desc)
+    def return_appearance(self, looker):
+        message = []
+        message.append(header(self.key))
+        message.append("|w_|n" * 78)
+        message.append("|[002|w{}{}|n\n".format(self.key, " " * (78 - len(self.key))))
+        message.append("|w-|n" * 78)
+        message.append(self.db.desc)
+        message.append("|{}_|n".format(clearance_color(CLEARANCE.get(self.db.clearance))) * 78)
+
+        chars = self.list_characters()
+        objects = self.list_non_characters()
+        colored_objects = []
+        for obj in objects:
+            colored_objects.append("|135%s|n" % obj.key)
+
+        exits = []
+        if self.exits:
+            for exit in self.exits:
+                if exit.access(looker, "view"):
+                    exits.append("|w<|n|b%s|n|w>|n - %s" % (exit.key, exit.destination))
+
+        table = evtable.EvTable("|wCharacters and Objects:|n", "|wExits:|n", table=[chars + colored_objects, exits],
+                                border=None)
+        table.reformat_column(0, width=28, align="l")
+
+        message.append(table)
+        message.append("\n")
+
+        message2 = []
+        for line in message:
+            message2.append(unicode(line))
+
+        return "\n".join(message2)
 
     def list_characters(self):
         return sorted([char for char in self.contents if char.is_typeclass(Clone, exact=False)])
