82ca5727f3455a94f981924b2f13ed118ce1b693
grungies1138 <grungies1138@gmail.com>
2018-08-28 09:48:22 -0500

    Finishing touches on chargen

--- commands/command.py
+++ commands/command.py
@@ -14,29 +14,6 @@ from world.static_data import HEALTH, CLEARANCE
 from django.conf import settings
 from evennia.server.sessionhandler import SESSIONS
 
-
-class Command(BaseCommand):
-    """
-    Inherit from this if you want to create your own command styles
-    from scratch.  Note that Evennia's default commands inherits from
-    MuxCommand instead.
-
-    Note that the class's `__doc__` string (this text) is
-    used by Evennia to create the automatic help entry for
-    the command, so make sure to document consistently here.
-
-    Each Command implements the following methods, called
-    in this order (only func() is actually required):
-        - at_pre_cmd(): If this returns True, execution is aborted.
-        - parse(): Should perform any extra parsing needed on self.args
-            and store the result on self.
-        - func(): Performs the actual work.
-        - at_post_cmd(): Extra actions, often things done after
-            every command, like prompts.
-
-    """
-    pass
-
 class SheetCommand(default_cmds.MuxCommand):
     """
     Displays the Character sheet.
