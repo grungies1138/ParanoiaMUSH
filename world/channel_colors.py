96e5758aa89ee95e81b260357a3ad42c3fa6407c
grungies1138 <grungies1138@gmail.com>
2019-07-16 13:48:17 -0500

    Correcting 0.9 issues

--- world/channel_colors.py
+++ world/channel_colors.py
@@ -1,4 +1,4 @@
 COLORS = {'Public': '|015Public|n',
           'Newbie': '|550N|n|551e|n|552w|n|553b|n|554i|n|555e|n',
           'Staff': '|010S|n|020t|n|030a|n|040f|n|050f|n',
-          'Admin': '|yAdmin|n'}
+          'Admin': '|440Admin|n'}
