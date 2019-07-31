95713d9be4fd23cb590668d8041d4c16a5e9f6bc
grungies1138 <grungies1138@gmail.com>
2019-07-31 12:36:22 -0500

    BBS

--- typeclasses/board.py
+++ typeclasses/board.py
@@ -56,6 +56,8 @@ class PostHandler(DefaultScript):
 
     def add(self, post):
         self.db.posts.append(post)
+        for sub in self.obj.db.subscribers:
+            sub.msg("|gSYSTEM:|n {} added to the {} board.".format(post.header, self.obj.key))
 
     def delete(self, post):
         self.db.posts.remove(post)
@@ -104,19 +106,23 @@ class BBReadCmd(default_cmds.MuxCommand):
             self.caller.msg("\n".join(str(m) for m in message2))
         elif "/" in self.args:
             pass
-        #else:
-            # board = list(Board.objects.all())[self.args]
-            # self.caller.msg("{} Posts".format(board.key))
-            # message = []
-            # table = evtable.EvTable("")
+        # else:
+        #     board = list(Board.objects.all())[self.args]
+        #     self.caller.msg("{} Posts".format(board.key))
+        #     message = []
+        #     table = evtable.EvTable("")
 
 
 class BBPostCmd(default_cmds.MuxCommand):
     """
     This command begins writing a post to the specified board.
 
+    The title will be limited to 60 characters.
+
     Usage:
         |w+bbpost <#>/<title>|n - Start a post on board <#> with the title of <title>.
+
+        |w+bbpost|n - to complete a post and add it to the board.
 
     """
     key = "+bbpost"
@@ -124,15 +130,51 @@ class BBPostCmd(default_cmds.MuxCommand):
     help_category = HELP_CATEGORY
 
     def func(self):
-        pass
+        if not self.args:
+            if not self.caller.db.post:
+                self.caller.msg("|gSYSTEM:|n You have not started a post yet.  See |whelp +bbpost|n for more info.")
+                return
+            temp_post = self.caller.db.post
+            message = temp_post.get("message")
+            board = temp_post.get("board")
+            header = temp_post.get("title")
+            post = create_message(self.caller, message, receivers=board, header=header)
+            board.posts.add(post)
+
+        else:
+            if "/" not in self.args:
+                self.caller.msg("|gSYSTEM:|n Invalid syntax.  Please try again.  See: |whelp +bbpost|n for help")
+                return
+
+            args = self.args.split("/")
+
+            board_ids = [b.id for b in list(Board.objects.all())]
+
+            if not int(args[0]) in board_ids:
+                self.caller.msg("|gSYSTEM:|n That is not a valid board ID.  Please try again.  See |whelp +bbread|n "
+                                "for a list of the boards.")
+                return
+
+            board = Board.objects.filter(key=int(args[0]))
+            if not board.access(self.caller, 'post'):
+                self.caller.msg("|gSYSTEM:|n You do not have permission to post to that board.  See |w+bbread|n to "
+                                "see a list of available boards.")
+                return
+
+            self.caller.db.post = {"title": self.args[1], "board": board}
+            self.caller.msg("|gSYSTEM:|n Post started.  type |w+bb <text>|n to add the post content.")
 
 
 class BBCmd(default_cmds.MuxCommand):
     """
-    Once a post is started, this command allows the user to add content to the post.
+    Once a post is started, this command allows the user to add content to the post.  To append an new
+    paragraph, just use this command a second time.  Each use will add a new line between each addition.
 
     Usage:
         |w+bb <text>|n - Add content to a currently started post.
+
+
+    See |wcolor ansi|n for carriage returns, tabs and other special characters.
     """
 
     key = "+bb"
@@ -140,7 +182,13 @@ class BBCmd(default_cmds.MuxCommand):
     help_category = HELP_CATEGORY
 
     def func(self):
-        pass
+        post = self.caller.db.post
+        message = post.get("message")
+        if not post:
+            self.caller.msg("|gSYSTEM:|n No BBS Post started.  See |whelp +bbpost|n for more info.")
+            return
+        post["message"] = "{}\n{}".format(message, self.args)
+        self.caller.msg("|gSYSTEM:|n Post updated.  |w+bbproof|n to proof read your post.")
 
 
 class BBProofCmd(default_cmds.MuxCommand):
